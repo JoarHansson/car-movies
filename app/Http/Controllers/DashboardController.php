c176fd9aabe2e41c8146955865d9b06ee16150aa
Frida Fernhede <fernhedefrida@gmail.com>
2024-02-27 14:38:07 +0100

    added api call

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Http;
 
 class DashboardController extends Controller
 {
     public function __invoke(Request $request)
     {
-
-        return view('dashboard', ['user' => Auth::user()]);
+        $ApiKey = env('APIKEY');
+        $response = Http::get('https://api.themoviedb.org/3/movie/550?api_key=' . $ApiKey);
+        unset($ApiKey);
+        $list = $response->body();
+        return view('dashboard', [
+            'user' => Auth::user(),
+            'movieList' => $list,
+        ]);
     }
 }
