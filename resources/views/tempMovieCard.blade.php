4182d9448cc4aa017ca19c52eea0c4f002585cf7
Frida Fernhede <fernhedefrida@gmail.com>
2024-02-29 15:17:17 +0100

    Added movies in discover-tab and randomized them

--- resources/views/tempMovieCard.blade.php
+++ resources/views/tempMovieCard.blade.php
@@ -1,1 +1,13 @@
-@dd($movieList)
+<!-- Generate a card for all movies -->
+
+<div class="grid">@isset($movieList)
+    @foreach( $movieList->results as $movie)
+    <div class="container">
+        <img src="http://image.tmdb.org/t/p/w500{{$movie->poster_path}}" alt="">
+        <h3>{{$movie->title}}</h3>
+        <p>{{$movie->vote_average}}</p>
+
+    </div>
+    @endforeach
+    @endisset
+</div>
