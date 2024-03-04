9c0476541a634933503c54d0ec2472ed56fba238
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-03-04 13:40:46 +0100

    make some small style changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,8 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
-<link rel="stylesheet" href="app.css">
+{{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" /> --}}
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.red.min.css"
+/>
 <div class="container">
 
     <!-- some basic Styling using Pico -->
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,29 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+{{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" /> --}}
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.red.min.css"
+/>
+
 <style>
-    :root {
-        --pico-border-radius: 1rem;
+    .container {
+        margin-top: 3rem;
     }
 
-    button {
-        --pico-font-weight: 700;
-    }
+    </style>
 
-    form,
-    button {
-        max-width: 80%;
-        margin-left: 10%;
+<div class="container">
 
-    }
+    <form action="/login" method="post">
+        @csrf
+        <div>
+            <label for="email">Email</label><input name="email" id="email" type="email" />
+        </div>
+        <div>
+            <label for="password">Password</label><input name="password" id="password" type="password" />
+        </div>
+        <button type="submit">Login</button>
+    </form>
 
-    form {
-        margin-top: 5rem;
-    }
-</style>
+    @include('errors')
 
-<form action="/login" method="post">@csrf <div><label for="email">Email</label><input name="email" id="email" type="email" /></div>
-    <div><label for="password">Password</label><input name="password" id="password" type="password" /></div><button type="submit">Login</button>
-</form>@include('errors')
+</div>
