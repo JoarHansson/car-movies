95cc253abf9578bc7cc8177221320209daa705f3
Frida <fernhedefrida@gmail.com>
2024-03-05 14:28:58 +0100

    Now you can only like the same movie once

--- app/Http/Controllers/AddLikesController.php
+++ app/Http/Controllers/AddLikesController.php
@@ -7,28 +7,39 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use App\Models\User;
 use App\Models\Like;
+use Illuminate\Support\Facades\DB;
 
 class AddLikesController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $id = $request->input('movieId');
-        $apiKey = env('API_KEY');
-        $response = Http::get('https://api.themoviedb.org/3/movie/' . $id . '?language=en-US&api_key=' . $apiKey);
-        $list = $response->object();
-        $like = new Like([
-            'movie_id' => $list->id,
-            'movie_title' => $list->title,
-            'movie_poster' => $list->poster_path,
-            'movie_rating' => $list->vote_average,
-        ]);
-
         $userId = Auth::id();
         $user = Auth::user();
-        $id = $user->id;
-        $like->user_id = $userId;
-        $userMethod = User::find($id);
-        $userMethod->likes()->save($like);
-        return redirect()->back();
+        $id = $request->input('movieId');
+        $apiKey = env('API_KEY');
+
+         if (DB::table('likes')->where([
+                ['movie_id', '=', $id],
+                ['user_id', '=', $userId],
+            ])->exists())
+            {
+                return redirect()->back();
+            } else {
+            $response = Http::get('https://api.themoviedb.org/3/movie/' . $id . '?language=en-US&api_key=' . $apiKey);
+            $list = $response->object();
+            $like = new Like([
+                'movie_id' => $list->id,
+                'movie_title' => $list->title,
+                'movie_poster' => $list->poster_path,
+                'movie_rating' => $list->vote_average,
+            ]);
+
+            $id = $user->id;
+            $like->user_id = $userId;
+            $userMethod = User::find($id);
+            $userMethod->likes()->save($like);
+            unset($apikey);
+            return redirect()->back();
+        }
     }
 }
--- app/Http/Controllers/GetMoviesController.php
+++ app/Http/Controllers/GetMoviesController.php
@@ -11,7 +11,13 @@ class GetMoviesController extends Controller
     public function __invoke(Request $request)
     {
         // get movies based on car racing as keyword
-        $randomInt = rand(1, 10);
+        $keywords= [
+            '310324',
+            '830-car-race',
+            '233981',
+            '286354',
+        ];
+        $randomInt = rand(1, 8);
         $apiKey = env('API_KEY');
         $response = Http::get('https://api.themoviedb.org/3/keyword/830-car-race/movies?include_adult=false&language=en-US&page=' . $randomInt . '&sort_by=popularity.desc&api_key=' . $apiKey);
         unset($apiKey);
