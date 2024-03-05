582931c238047e71c7af4a09496e30219e5c0c54
Frida <fernhedefrida@gmail.com>
2024-03-05 15:41:22 +0100

    Added working like/dislike-toggle and flexible desktop

--- app/Http/Controllers/GetMoviesController.php
+++ app/Http/Controllers/GetMoviesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 class GetMoviesController extends Controller
 {
@@ -17,6 +18,7 @@ class GetMoviesController extends Controller
             '233981',
             '286354',
         ];
+        
         $randomInt = rand(1, 8);
         $apiKey = env('API_KEY');
         $response = Http::get('https://api.themoviedb.org/3/keyword/830-car-race/movies?include_adult=false&language=en-US&page=' . $randomInt . '&sort_by=popularity.desc&api_key=' . $apiKey);
--- resources/views/likes.blade.php
+++ resources/views/likes.blade.php
@@ -5,6 +5,12 @@
         <img src="http://image.tmdb.org/t/p/w500{{$movie->movie_poster}}" alt="">
         <h3>{{$movie->movie_title}}</h3>
         <p>{{$movie->movie_rating}}</p>
+        <h3>{{$movie->movie_title}}</h3>
+        <form action="/manageLike">
+            <input type="hidden" value="{{$movie->movie_id}}">
+            <button type="submit" name="movieId" value="{{$movie->movie_id}}"> Like </button>  
+        </button>
+        </form>
     </div>
     @endforeach
     @endisset
