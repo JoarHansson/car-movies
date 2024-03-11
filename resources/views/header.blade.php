5846d0cdb6eec2af3cb4c81d4a87f78bd254b716
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-03-11 13:34:10 +0100

    small link changes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <header class="header">
-    <h3><a href="/" class="contrast">Car Movies</a></h3>
+    <h3><a href="/getToplist" class="contrast">Car Movies</a></h3>
     <div class="nav">
         <a href="/accountManager" class="contrast">My account</a>
         <a href="/logout" class="contrast">Logout</a>
